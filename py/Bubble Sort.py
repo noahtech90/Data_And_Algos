50f56aad998b97818a8152e1ee14a8528c10df01
Noah <noahhodge2@gmail.com>
2020-09-19 17:28:01 -0700

    Bubble Sort

--- py/Bubble Sort.py
+++ py/Bubble Sort.py
@@ -9,19 +9,19 @@ Switch each pair which shows the next index containing a value less than the cur
 
 array = [5, 9, 22, 0, 64, 203, 44, 56, 2]
 
+"Takes in array as input and orders it by increasing value"
 
 def bubble_sort(arr):
     length = len(array)
 
+    'Iterate through entire array'
     for i in range(length):
 
-
+        'Check each value vs the value in front of it'
         for j in range(length - i - 1):
 
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
-
-
     return arr
 
 
