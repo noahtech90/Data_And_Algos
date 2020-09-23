1b237df304c4727b9fb1b57b097661cb53d2ef41
Noah <noahhodge2@gmail.com>
2020-09-23 16:45:26 -0700

    Javascript Binary Search Tree

---
+++ js/main.js
@@ -0,0 +1,35 @@
+class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null
+    }
+    add(data) {
+        const node = this.root;
+        if (node === null) {
+            node.left = new Node(data);
+            return;
+        }
+        else {
+            const searchTree = function(node) {
+                if (data < node.data) {
+                    if (node.left == null) {
+                        node.left = new Node(data);
+                        return
+                    }
+                    
+                }
+            }
+        }
+    }
+
+
+
+
+}
