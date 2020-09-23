aa9179f0abf915a871c6dc5236b4c0c60736ee3f
Noah <noahhodge2@gmail.com>
2020-09-23 16:35:10 -0700

    Graph Find Path

--- py/graph.py
+++ py/graph.py
@@ -15,14 +15,17 @@ graph = { 'A': ['B', 'C'],
           'F': ['C']
         }
 
+
 def find_path(graph, start, end, path=[]):
     path = path + [start]
     if start == end:
         return path
-    if not graph.has_key(start):
+    if start not in graph:
         return None
     for node in graph[start]:
         if node not in path:
             newpath = find_path(graph, node, end, path)
             if newpath: return newpath
-        return None
+    return None
+
+print(find_path(graph, 'A', 'C'))
